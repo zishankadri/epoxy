1a9b98e1a84619a1d9da1dec6552c046ff563adb
Blackjoker52 <zishankadri9@gmail.com>
2024-06-26 22:03:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './*/templates/**/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand': '#DEAAF2',
+        'brand-hv': '#EFC0F9',
+        'brand-light':'#F9DFFF',
+        'brand-dark': '#201A34',
+        'brand-dark-xl': '#161E22',
+      },
+    },
+    fontFamily: {
+      'poppins': ['Poppins'],
+      'handlee': ['Handlee'],
+      'pangolin': ['Pangolin'],
+   }
+  },
+  plugins: [],
+}
+
