bcc6d1b045315bc2dbff28e911ca08df0ab5c819
Blackjoker52 <zishankadri9@gmail.com>
2024-07-07 01:31:23 +0530

    updates

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('gallery/', views.gallery, name="gallery"),
 
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-def home(request):
-    
+def home(request): 
     return render(request, "home.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
